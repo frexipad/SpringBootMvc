7f16c57cf16f1cb51568753afc6b2214dcdcb416
frexdel <frexdel@gmail.com>
2019-05-26 17:05:03 +0200

    Add resource  Quote.Class
    its Body

--- src/main/java/com/frexdel/test/model/Quote.java
+++ src/main/java/com/frexdel/test/model/Quote.java
@@ -1,4 +1,25 @@
 package com.frexdel.test.model;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "quote",catalog = "test")
 public class Quote {
+    
+    public Quote(String userName, String quote) {
+        this.userName = userName;
+        this.quote = quote;
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Integer id;
+    @Column(name = "username")
+    private String userName;
+    @Column(name = "quote")
+    private String quote;
 }
